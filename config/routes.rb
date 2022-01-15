901b1a44ed1009e3b13a8541b4d376ec16802389
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-01-15 15:10:16 +0900

    rails g devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "materials#index"
   resources :materials, only: [:index, :new, :create, :edit, :update, :destroy] do
     collection do
