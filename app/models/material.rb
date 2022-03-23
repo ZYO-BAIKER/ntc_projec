1c0e319fb259be81f1397c499abcd8070da80058
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-03-24 03:00:19 +0900

    場所選択のActiveHash作成

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,8 @@
 class Material < ApplicationRecord
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user_select
+  belongs_to :place_select
 
   validates :name, :place, :user, :period_start, :memo, presence: true # 必須設定
 end
