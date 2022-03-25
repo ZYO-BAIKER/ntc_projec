2c1f05de8be7e203cca24ac5742d08bdafa9b36e
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-03-25 09:14:28 +0900

    material_modelのテストコード実装

--- spec/factories/materials.rb
+++ spec/factories/materials.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :material do
+    material_name { Faker::Lorem.word }
+    material_count { Faker::Number.number }
+    maker { Faker::Name.initials }
+    place { Faker::Address.state }
+    user { Faker::Name.name }
+    period_start { Time.zone.now.strftime("%Y-%m-%d") }
+    memo { Faker::Lorem.sentence }
   end
 end
--- spec/models/material_spec.rb
+++ spec/models/material_spec.rb
@@ -1,5 +1,68 @@
 require "rails_helper"
 
 RSpec.describe Material, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系" do
+    context "必要な情報が揃っている場合" do
+      let(:material) { build(:material) }
+
+      it "資材情報が登録される" do
+        expect(material).to be_valid
+      end
+    end
+  end
+
+  describe "異常系" do
+    context "material_nameがない場合" do
+      let(:material) { build(:material, material_name: nil) }
+
+      it "エラーする" do
+        expect(material).not_to be_valid
+      end
+    end
+
+    context "material_countに異常" do
+      let(:material) { build(:material, material_count: nil) }
+
+      it "空欄でエラーする" do
+        expect(material).not_to be_valid
+      end
+
+      it "全角で入力でエラーする" do
+        material.material_count = "１２３２"
+        expect(material).not_to be_valid
+      end
+    end
+
+    context "placeがない場合" do
+      let(:material) { build(:material, place: nil) }
+
+      it "エラーする" do
+        expect(material).not_to be_valid
+      end
+    end
+
+    context "userがない場合" do
+      let(:material) { build(:material, user: nil) }
+
+      it "エラーする" do
+        expect(material).not_to be_valid
+      end
+    end
+
+    context "period_startがない場合" do
+      let(:material) { build(:material, period_start: nil) }
+
+      it "エラーする" do
+        expect(material).not_to be_valid
+      end
+    end
+
+    context "memoがない場合" do
+      let(:material) { build(:material, memo: nil) }
+
+      it "エラーする" do
+        expect(material).not_to be_valid
+      end
+    end
+  end
 end
