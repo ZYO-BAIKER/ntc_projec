c74537dd0b16201d2d4b62e3ad66d80bbeb649f0
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-03-31 20:12:53 +0900

    登録可能まで
    画面とロジック
    バリデーションは保留

--- app/javascript/register_select.js
+++ app/javascript/register_select.js
@@ -1,21 +1,5 @@
-// case1
+// 現場1
 $(function() {
-  // 「現場」選択時、現場入力欄表示
-  $('#material_place').change(function() {
-    if ($("#material_place").val() === '現場')
-      $(".other_places-input").show();
-    else
-      $(".other_places-input").hide();
-  });
-
-  // 「修理中」選択時、修理中入力欄表示
-  $('#material_place').change(function() {
-    if ($("#material_place").val() === '修理中')
-      $(".repair_request-input").show();
-    else
-      $(".repair_request-input").hide();
-  });
-
   // 「その他」選択時、その他使用者の入力欄表示
   $('#material_user').change(function() {
     if ($("#material_user").val() === 'その他')
@@ -25,56 +9,24 @@ $(function() {
   });
 });
 
-// case2
+// 現場2
 $(function() {
-  // 「現場」選択時、現場入力欄表示
-  $('#material_place_2').change(function() {
-    if ($("#material_place_2").val() === '現場')
-      $(".other_places-input_2").show();
-    else
-      $(".other_places-input_2").hide();
-  });
-
-  // 「修理中」選択時、修理中入力欄表示
-  $('#material_place_2').change(function() {
-    if ($("#material_place_2").val() === '修理中')
-      $(".repair_request-input_2").show();
-    else
-      $(".repair_request-input_2").hide();
-  });
-
   // 「その他」選択時、その他使用者の入力欄表示
-  $('#material_user_2').change(function() {
-    if ($("#material_user_2").val() === 'その他')
-      $(".other_users-input_2").show();
+  $('#material_user2').change(function() {
+    if ($("#material_user2").val() === 'その他')
+      $(".other_users-input2").show();
     else
-      $(".other_users-input_2").hide();
+      $(".other_users-input2").hide();
   });
 });
 
-// case3
+// 現場3
 $(function() {
-  // 「現場」選択時、現場入力欄表示
-  $('#material_place_3').change(function() {
-    if ($("#material_place_3").val() === '現場')
-      $(".other_places-input_3").show();
-    else
-      $(".other_places-input_3").hide();
-  });
-
-  // 「修理中」選択時、修理中入力欄表示
-  $('#material_place_3').change(function() {
-    if ($("#material_place_3").val() === '修理中')
-      $(".repair_request-input_3").show();
-    else
-      $(".repair_request-input_3").hide();
-  });
-
   // 「その他」選択時、その他使用者の入力欄表示
-  $('#material_user_3').change(function() {
-    if ($("#material_user_3").val() === 'その他')
-      $(".other_users-input_3").show();
+  $('#material_user3').change(function() {
+    if ($("#material_user3").val() === 'その他')
+      $(".other_users-input3").show();
     else
-      $(".other_users-input_3").hide();
+      $(".other_users-input3").hide();
   });
 });
--- app/models/material.rb
+++ app/models/material.rb
@@ -5,6 +5,6 @@ class Material < ApplicationRecord
   validates :material_name, :all_count, presence: true # 必須設定
 
   with_options numericality: { with: /\A[0-9]+\z/, message: "は半角で入力して下さい" } do
-    validates :all_count, :garage_count, :garage_count2, :garage_count3, :use_count, :use_count2, :use_count3, :repair_count
+    validates :all_count
   end
 end
