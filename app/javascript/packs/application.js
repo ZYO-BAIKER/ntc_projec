6c370ce52e2ba558bf52c9ddde1a96fc21df35e7
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-04-07 06:58:57 +0900

    追記

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,10 @@ require('jquery')
 require("../header")
 require("../register_select")
 
+
+import "bootstrap"; //追加
+import "../stylesheets/application"; //追加
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
