5dac232c9d3aaadeeecdcebe10c77c7097a33b84
ZYO-BAIKER <keisukeminakawa@gmail.com>
2023-05-08 01:32:52 +0900

    new作成

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,34 @@
 class AttendancesController < ApplicationController
+  before_action :set_attendance, only: [:edit, :update, :destroy]
+
   def index
     @attendances = Attendance.all.order("created_at DESC").page(params[:page]).per(10)
+  end
+
+  def new
+    @attendance = Attendance.new
+  end
+
+  def create
+    @attendance = Attendance.new(attendance_params)
+    if @attendance.valid?
+      @attendance.save!
+      redirect_to attendances_path
+    else
+      render :new
+    end
+  end
+
+  def top
+  end
+
+  private
+
+  def set_attendance
+    @attendance = Attendance.find(params[:id])
+  end
+
+  def attendance_params
+    params.require(:attendance).permit(:client,:construction_site,:departure_time,:arrival_time,:remark,:vehicle)
   end
 end
