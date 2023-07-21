11b8b70e8dae6a432b3511f490d9039b41a88250
ZYO-BAIKER <keisukeminakawa@gmail.com>
2023-07-22 00:21:32 +0900

    資材ヘッダーリファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def header_link(label, path, icon, condition = nil, options = {})
+    unless condition
+      content_tag :li do
+        link_to label, path, options.merge({class: "fas #{icon} text-decoration-none link-dark"})
+      end
+    end
+  end
 end
