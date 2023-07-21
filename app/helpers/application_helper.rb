17007349101144692023b40ad055ef11911ed38a
ZYO-BAIKER <keisukeminakawa@gmail.com>
2023-07-22 00:22:50 +0900

    bundle exec rubocop -A

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def header_link(label, path, icon, condition = nil, options = {})
     unless condition
       content_tag :li do
-        link_to label, path, options.merge({class: "fas #{icon} text-decoration-none link-dark"})
+        link_to label, path, options.merge({ class: "fas #{icon} text-decoration-none link-dark" })
       end
     end
   end
