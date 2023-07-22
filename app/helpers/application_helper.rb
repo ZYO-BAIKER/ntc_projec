cb7f5dbf66d8c1547553a243e724fc407124a98c
ZYO-BAIKER <keisukeminakawa@gmail.com>
2023-07-22 23:56:22 +0900

    ヘッダー改修

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def header_link(options = {})
-    unless options[:condition]
-      content_tag :li do
-        link_to options[:label], options[:path],
-                options[:link_options].merge({ class: "fas #{options[:icon]} text-decoration-none #{options[:link_class]}" })
-      end
-    end
-  end
 end
