951eda8e6163ee74d30a9fdfcfb0424be175bd47
ZYO-BAIKER <keisukeminakawa@gmail.com>
2023-07-22 16:04:22 +0900

    レスポンシブのレイアウト訂正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def header_link(label, path, icon, condition = nil, options = {})
+  def header_link(label, path, icon, condition = nil, options = {}, link_class = "link-dark")
     unless condition
       content_tag :li do
-        link_to label, path, options.merge({ class: "fas #{icon} text-decoration-none link-dark" })
+        link_to label, path, options.merge({ class: "fas #{icon} text-decoration-none #{link_class}" })
       end
     end
   end
