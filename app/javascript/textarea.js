922490207634ca543b5fe3ca0e6a01708bb9c97b
ZYO-BAIKER <keisukeminakawa@gmail.com>
2023-08-12 10:51:44 +0900

    表示も折り返されるように

--- app/javascript/textarea.js
+++ app/javascript/textarea.js
@@ -1,3 +1,14 @@
+function adjustTextareaHeight(textareaElement) {
+  $(textareaElement).height(0).height(textareaElement.scrollHeight);
+}
+// 入力時にtextareaの高さを調整
 $(document).on('input', 'textarea', function () {
-  $(this).height(0).height(this.scrollHeight);
+  adjustTextareaHeight(this);
+});
+
+// ページ読み込み時にも全てのtextareaの高さを調整
+$(document).ready(function() {
+  $('textarea').each(function() {
+      adjustTextareaHeight(this);
+  });
 });
